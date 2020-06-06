1804d03ec0d061aedd2ebe0e1b2f8afc9d87aff9
David Okunade <davidokunade1@gmail.com>
2020-06-06 11:19:03 +0100

    finished the search component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import SearchBox from "./SearchBox";
+import DogList from "./DogList";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const App = () => {
+// [dogs, setDogs] = useState([
+//  {name:"Max", breed: "Lab"},
+//  {name:"Sparky", breed: "German Sherperd"},
+// {name:"Rex", breed: "Boxer"},
+//// {name:"Sally", breed: "Poodle"},
+// {name:"George", breed: "Pitbull"}
+//])
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogs: [
+        { name: "Max", breed: "Lab" },
+        { name: "Sparky", breed: "German Sherperd" },
+        { name: "Rex", breed: "Boxer" },
+        { name: "Sally", breed: "Poodle" },
+        { name: "George", breed: "Pitbull" },
+      ],
+      searchDog: "",
+    };
+  }
+  handleInput = (e) => {
+    this.setState({ searchDog: e.target.value });
+  };
+  render() {
+    let filteredDogs = this.state.dogs.filter((dog) => {
+      return dog.name
+        .toLowerCase()
+        .includes(this.state.searchDog.toLowerCase());
+    });
+    return (
+      <div className="App">
+        <h1> Dog Images</h1>
+        <SearchBox handleInput={this.handleInput} />
+        <DogList filteredDogs={filteredDogs} />
+      </div>
+    );
+  }
 }
 
 export default App;
